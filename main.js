c2ea4ed11763dee78797c00705e2380a5ce9e947
Atosvini <139802273+atosvini@users.noreply.github.com>
2023-08-07 16:15:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function soma(a,b) {
+if (a+b === 50) {
+    console.log(( a + b) + " e igual a 50.");
+} else {
+console.log ( (a + b ) +  "nao e igual a 50.");
+}
+ 
+
+}
+console.log(soma(25,25))
